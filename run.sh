547a24a0a407369130d0fdccb98041f26134946c
jingdong.jared.chen <magical_kingdom@126.com>
2025-12-07 21:41:40 +0800

    Refactor run.sh to include Docker login, tagging, and pushing steps

--- run.sh
+++ run.sh
@@ -6,3 +6,13 @@ docker build --no-cache -t fullstack-dev-env:{version} .
 
 # Save the Docker image to a tar file
 docker save -o fullstack-dev-env_${version}.tar fullstack-dev-env:${version}
+
+# ======================================================
+# docker login
+docker login
+
+# tag the image
+docker tag fullstack-dev-env:${version} your_dockerhub_username/fullstack-dev-env:${version}
+
+# push the image to Docker Hub
+docker push your_dockerhub_username/fullstack-dev-env:${version}
