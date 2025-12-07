724095694b1ce940911de4e515f86754ebb017ac
jingdong.jared.chen <magical_kingdom@126.com>
2025-12-07 21:36:30 +0800

    Add versioning and comments to run.sh for clarity

--- run.sh
+++ run.sh
@@ -1,1 +1,8 @@
+# !/bin/bash
+version="v1.0.0"
+
+# Build the Docker image without using cache
 docker build --no-cache -t fullstack-dev-env:{version} .
+
+# Save the Docker image to a tar file
+docker save -o fullstack-dev-env_${version}.tar fullstack-dev-env:${version}
